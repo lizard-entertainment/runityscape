17232e358a6cde22a9df7a1a2130c625b354fc4c
Andrew X <me@andrewxue.com>
2017-11-16 22:45:08 -0500

    flan's armor more expensive

--- Assets/Scripts/Game/Defined/Serialized/Items/Areas/RuinsItems.cs
+++ Assets/Scripts/Game/Defined/Serialized/Items/Areas/RuinsItems.cs
@@ -210,7 +210,7 @@ namespace Scripts.Game.Defined.Serialized.Items {
         public RevivalSeed()
             : base(
                   "acorn",
-                  200,
+                  100,
                   TargetType.ONE_ALLY,
                   "Revival Seed",
                   string.Format("Use on an ally to restore them by {0}% of their missing {1}. Can be used on fallen allies.",
@@ -255,7 +255,7 @@ namespace Scripts.Game.Defined.Serialized.Items {
 
     public class RegenArmor : EquippableItem {
 
-        public RegenArmor() : base("chain-mail", EquipType.ARMOR, 200, "Flan's Mail", "A soothing mail that heals wounds. Smells like eggs.") {
+        public RegenArmor() : base("chain-mail", EquipType.ARMOR, 500, "Flan's Mail", "A soothing mail that heals wounds. Smells like eggs.") {
             AddFlatStatBonus(StatType.AGILITY, -5);
             AddFlatStatBonus(StatType.VITALITY, 5);
         }
